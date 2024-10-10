a685c75e8d5db15c73dbc52799068c389d6aa32e
kozlovanvk <166809988+kozlovanvk@users.noreply.github.com>
2024-10-11 01:03:27 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,13 @@
 let chekStringLength = function (string, maxLength) {
-  if (string.length > supposedLength) {
+  if (string.length > maxLength) {
     return false;
   }
   return true;
 };
 
 let chekPalindrome = function (string) {
-  stringWithoutSpaces = string.replaceAll(' ', '');
-  reference = stringWithoutSpaces.toLowerCase();
+  let stringWithoutSpaces = string.replaceAll(' ', '');
+  let reference = stringWithoutSpaces.toLowerCase();
   let reverseString = '';
   for (let i = reference.length - 1; i >= 0; i--) {
     reverseString += reference[i];
