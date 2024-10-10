2b73828519722aeaa45a670ee18138c314767837
kozlovanvk <166809988+kozlovanvk@users.noreply.github.com>
2024-10-11 00:53:52 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,14 @@
+let chekStringLength = function (string, maxLength) {
+  if (string.length > supposedLength) return false;
+  return true;
+};
 
+let chekPalindrome = function (string) {
+  stringWithoutSpaces = string.replaceAll(' ', '');
+  reference = stringWithoutSpaces.toLowerCase();
+  let reverseString = '';
+  for (let i = reference.length - 1; i >= 0; i--) {
+    reverseString += reference[i];
+  }
+  return reverseString === reference;
+}
