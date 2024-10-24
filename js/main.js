66a711d0892a8f26c03041ea3857122b7df5ecbd
kozlovanvk <166809988+kozlovanvk@users.noreply.github.com>
2024-10-24 18:06:34 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import './util.js';
-import {posts} from './data.js';
-
+import {createPost} from './data.js';
+//Создание постов можно было перенести в data.js, 
+//но ругается проверка, что не используется posts, и что её нельзя вывести в консоль, я сделала пока так
+const posts = [];
+for (let i = 0; i < 25; i++) {
+  posts.push(createPost());
+}
