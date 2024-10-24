b51e61969ee30c1b1be6b959913b8c19f2e15efe
kozlovanvk <166809988+kozlovanvk@users.noreply.github.com>
2024-10-24 19:46:22 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -8,15 +8,14 @@ const canHaveMeeting = function (startWorkday, endWorkday, startMeeting, meeting
 
   startMeeting = startMeeting.split(':');
 
-  let endMeeting = [Number(startMeeting[0]) + parseInt(meetingLength / 60), meetingLength - parseInt(meetingLength / 60) * 60];
+  let endMeeting = [Number(startMeeting[0]) + parseInt(meetingLength / 60, 10), meetingLength - parseInt(meetingLength / 60, 10) * 60];
   endMeeting = endMeeting[0] + endMeeting[1] / 60;
 
   startMeeting = Number(startMeeting[0]) + Number(startMeeting[1] / 60);
 
   if (startWorkday <= startMeeting & startMeeting < endWorkday & endMeeting <= endWorkday) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 };
